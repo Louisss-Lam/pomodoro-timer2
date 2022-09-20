cd56e0c8a813cd3bdf380159013e62d03939725d
Louisss-Lam <louislam617@gmail.com>
2022-09-20 16:14:39 +0100

    feat: add three button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import SetPomodoro from "./components/SetPomodoro";
+import { SettingsContext } from "./context/SettingsContext";
+import { useContext } from "react";
+import Button from "./components/Button";
 
 function App() {
+  const {pomodoro, executeing, setCurrentTimer} = useContext(SettingsContext);
   return (
     <div className="container">
       <h1>Pomodoro</h1>
       <small>Be productive the right way</small>
-      <SetPomodoro />
+      {pomodoro !== 0 ?
+        <SetPomodoro /> :
+        <>
+          <ul className='labels'>
+            <li>
+              <Button
+                title='work'
+                activeClass={executeing.active === 'work' && 'active-label'}
+                _callback={() => setCurrentTimer('work')}
+              />
+            </li>
+            <li>
+              <Button
+                title='Short Break'
+                activeClass={executeing.active === 'work' && 'active-label'}
+                _callback={() => setCurrentTimer('short')}
+              />
+            </li>
+            <li>
+              <Button
+                title='Long Break'
+                activeClass={executeing.active === 'work' && 'active-label'}
+                _callback={() => setCurrentTimer('long')}
+              />
+            </li>
+          </ul>
+        </>
+      }
       {/* <CountdownCircleTimer /> */}
     </div>
   );
